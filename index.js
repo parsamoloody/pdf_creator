3e8789fff41d0e0ffc6d2650828cc8bfd69889a5
parsamoloody <parsaprogramerjs@gmail.com>
2025-06-04 21:56:18 +0330

    i fiexed some issue

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-import fs from 'node:fs'
-import PDFDocument from 'pdfkit'
+import fs from 'node:fs';
+import { mkdir } from 'node:fs/promises';
+import PDFDocument from 'pdfkit';
 import db from './db.js';
 
 const doc = new PDFDocument();
-doc.pipe(fs.createWriteStream('./PDF/new.pdf'))
+const pdfPath = new URL('./PDF', import.meta.url).pathname;
 
-doc.fontSize(8);
-doc.text(`Hi, i am ${db.person.name}`, {
-    width: 410,
-    align: 'left'
+if (!fs.existsSync(pdfPath) || !fs.lstatSync(pdfPath).isDirectory()) {
+  await mkdir(pdfPath, { recursive: true });
 }
-);
-doc.table({
-    data: [
-        (function () {
-            for (let i = 0; i <= db.skills.length; i++) {
-                console.log(db.skills[i])
-            }
-        })
-    ]
-})
 
+doc.pipe(fs.createWriteStream(`${pdfPath}/new.pdf`));
 
+doc.fontSize(8).text(`Hi, I am ${db.person.name}`, {
+  width: 410,
+  align: 'left',
+});
+
+doc.moveDown();
+doc.text('Skills:');
+db.skills.forEach((skill, i) => {
+  doc.text(`- ${skill}`);
+});
+
+doc.end();
